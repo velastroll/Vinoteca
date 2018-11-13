a1c4d6fe735d4646d41db3c04123f5799ea3d662
alvvela <velastroll@gmail.com>
2018-11-13 22:38:20 +0100

    GestionProductosRemote

---
+++ EJBRemote/src/dominio/Preferencia.java
@@ -0,0 +1,104 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package dominio;
+
+import dominio.Categoria;
+import dominio.DenominacionOrigen;
+import java.io.Serializable;
+import javax.persistence.Basic;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
+import javax.persistence.Table;
+import javax.xml.bind.annotation.XmlRootElement;
+
+/**
+ *
+ * @author Alvaro
+ */
+@Entity
+@Table(name = "PREFERENCIA")
+@XmlRootElement
+@NamedQueries({
+    @NamedQuery(name = "Preferencia.findAll", query = "SELECT p FROM Preferencia p"),
+    @NamedQuery(name = "Preferencia.findById", query = "SELECT p FROM Preferencia p WHERE p.id = :id")})
+public class Preferencia implements Serializable {
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Basic(optional = false)
+    @Column(name = "ID", nullable = false)
+    private Integer id;
+    @JoinColumn(name = "CATEGORIA", referencedColumnName = "CLAVE", nullable = false)
+    @ManyToOne(optional = false)
+    private Categoria categoria;
+    @JoinColumn(name = "IDDENOMINACION", referencedColumnName = "DO_ID", nullable = false)
+    @ManyToOne(optional = false)
+    private DenominacionOrigen iddenominacion;
+
+    public Preferencia() {
+    }
+
+    public Preferencia(Integer id) {
+        this.id = id;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Categoria getCategoria() {
+        return categoria;
+    }
+
+    public void setCategoria(Categoria categoria) {
+        this.categoria = categoria;
+    }
+
+    public DenominacionOrigen getIddenominacion() {
+        return iddenominacion;
+    }
+
+    public void setIddenominacion(DenominacionOrigen iddenominacion) {
+        this.iddenominacion = iddenominacion;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (id != null ? id.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof Preferencia)) {
+            return false;
+        }
+        Preferencia other = (Preferencia) object;
+        if ((this.id == null && other.id != null) || (this.id != null && !this.id.equals(other.id))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "dominio.Preferencia[ id=" + id + " ]";
+    }
+    
+}
---
+++ EJBRemote/src/sessionEJB/GestionProductosRemote.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sessionEJB;
+
+import dominio.Preferencia;
+import dominio.Referencia;
+import dominio.Vino;
+import java.util.List;
+import javax.ejb.Remote;
+
+/**
+ *
+ * @author Alvaro
+ */
+@Remote
+public interface GestionProductosRemote {
+    
+    List<Vino> getVinos(String categoria, String denOrigen);
+    
+    List<Referencia> getReferencias(int vinoId);
+    
+    List<Preferencia> getPreferencias(String login);
+    
+}
