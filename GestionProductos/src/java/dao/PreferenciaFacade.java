0e7d8c798c3d875f96822220baf3e44f70488e8e
alvvela <velastroll@gmail.com>
2018-11-13 22:37:21 +0100

    GestionProductos NOT TESTED

---
+++ GestionProductos/src/java/dao/PreferenciaFacade.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package dao;
+
+import dominio.Preferencia;
+import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+/**
+ *
+ * @author Alvaro
+ */
+@Stateless
+public class PreferenciaFacade extends AbstractFacade<Preferencia> implements PreferenciaFacadeLocal {
+    @PersistenceContext(unitName = "GestionProductosPU")
+    private EntityManager em;
+
+    @Override
+    protected EntityManager getEntityManager() {
+        return em;
+    }
+
+    public PreferenciaFacade() {
+        super(Preferencia.class);
+    }
+    
+}
---
+++ GestionProductos/src/java/sessionEJB/GestionProductos.java
@@ -0,0 +1,64 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sessionEJB;
+
+import dao.PreferenciaFacadeLocal;
+import dao.VinoFacadeLocal;
+import dominio.Preferencia;
+import dominio.Referencia;
+import dominio.Vino;
+import java.util.ArrayList;
+import java.util.List;
+import javax.ejb.EJB;
+import javax.ejb.Stateless;
+
+/**
+ *
+ * @author Alvaro
+ */
+@Stateless
+public class GestionProductos implements GestionProductosRemote {
+    @EJB
+    private PreferenciaFacadeLocal preferenciaFacade;
+    @EJB
+    private VinoFacadeLocal vinoFacade;
+    
+
+    @Override
+    public List<Vino> getVinos(String categoria, String denOrigen) {
+        List<Vino> wine = vinoFacade.findAll();
+        ArrayList<Vino> wineToReturn = new ArrayList();
+        String category, denOr;
+        for (Vino w : wine) {
+            category = w.getCategoria().toString();
+            denOr = w.getIddenominacion().getNombre();
+            if (category.equalsIgnoreCase(categoria) && denOr.equalsIgnoreCase(denOrigen))
+                wineToReturn.add(w);
+        }
+        return wineToReturn;
+    }
+
+    @Override
+    public List<Referencia> getReferencias(int vinoId) {
+        Vino wine = vinoFacade.find(vinoId);
+        return (List<Referencia>) wine.getReferenciaCollection();
+    }
+
+    @Override
+    public List<Preferencia> getPreferencias(String login) {
+        List<Preferencia> preferences = preferenciaFacade.findAll();
+        List<Preferencia> prefToReturn = new ArrayList();
+        for(Preferencia p: preferences){
+            String prefLogin = p.getNifabonado().getAbLogin();
+            if (prefLogin.equalsIgnoreCase(login))
+                prefToReturn.add(p);
+        }
+        return prefToReturn;
+    }
+
+    // Add business logic below. (Right-click in editor and choose
+    // "Insert Code > Add Business Method")
+}
